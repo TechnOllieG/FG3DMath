4b93f20cc0b80a301528da74e558bc63e8b672a1
TechnOllieG <56596714+TechnOllieG@users.noreply.github.com>
2020-09-23 21:23:51 +0200

    Added some of the solutions

---
+++ Assets/2. LookAtTrigger/LookAtTrigger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class LookAtTrigger : MonoBehaviour
+{
+    public Transform player;
+    [Range(0f, 0.99f)]
+    public float preciseness;
+
+    private void OnDrawGizmos()
+    {
+        Vector2 lookDirection = player.right;
+        Vector2 directionToTarget = (transform.position - player.position).normalized;
+        float currentPreciseness = Vector2.Dot(lookDirection, directionToTarget);
+
+        if (currentPreciseness > preciseness)
+        {
+            Handles.color = Color.green;
+        }
+        else
+        {
+            Handles.color = Color.red;
+        }
+        
+        Handles.DrawLine(player.position, player.position + (Vector3)lookDirection);
+    }
+}
