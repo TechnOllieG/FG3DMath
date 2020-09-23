c3dfa25d2f10ec9b6051f5ead0bb44968f3cecfe
TechnOllieG <56596714+TechnOllieG@users.noreply.github.com>
2020-09-23 16:22:37 +0200

    Import project

---
+++ Assets/RadialTrigger/RadialTrigger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class RadialTrigger : MonoBehaviour
+{
+    public float radius = 30f;
+    public Transform player;
+
+    private void OnDrawGizmos()
+    {
+        float distance = Vector2.Distance(player.position, transform.position);
+
+        if (distance > radius)
+        {
+            Handles.color = Color.red;
+        }
+        else
+        {
+            Handles.color = Color.green;
+        }
+
+        Handles.DrawWireDisc(transform.position, transform.forward, radius);
+    }
+}
